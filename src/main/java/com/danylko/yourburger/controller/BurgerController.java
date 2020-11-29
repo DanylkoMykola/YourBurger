03291bb56166870bccd1f0ad474a8f6f82fafd3f
DanylkoMykola <danylkomykola@gmail.com>
2020-11-29 18:30:51 +0300

    Work on BurgerController

--- src/main/java/com/danylko/yourburger/controller/BurgerController.java
+++ src/main/java/com/danylko/yourburger/controller/BurgerController.java
@@ -4,11 +4,12 @@ import com.danylko.yourburger.entities.Burger;
 import com.danylko.yourburger.service.BurgerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping(name = "/burgers")
@@ -17,13 +18,40 @@ public class BurgerController {
 
     private Logger log = LoggerFactory.getLogger(BurgerController.class);
 
+    @Autowired
     private BurgerService burgerService;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String list(Model uiModel) {
         List<Burger> burgers = burgerService.findAll();
         uiModel.addAttribute("burgers", burgers);
         return "burgers/list";
+    }
+
+    @GetMapping(value = "/{id}")
+    public String show(@PathVariable("id") Long id, Model uiModel) {
+        Burger burger = burgerService.findById(id);
+        uiModel.addAttribute("burger", burger);
+        return "show";
+    }
+
+    @GetMapping(name = "/edit/{id}")
+    public String updateForm(@PathVariable Long id, Model uiModel) {
+        uiModel.addAttribute("burger", burgerService.findById(id));
+        return "update";
+    }
+
+    @GetMapping(name = "/new")
+    public String createForm(Model uiModel) {
+        Burger burger = new Burger();
+        uiModel.addAttribute("burger", burger);
+        return "update";
+    }
+
+    @PostMapping
+    public String saveBurger(@Valid Burger burger) {
+        burgerService.save(burger);
+        return "redirect:/burgers/" + burger.getBurId();
     }
 
 
