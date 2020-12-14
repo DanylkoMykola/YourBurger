049b762499fc7ff06145de849a41109dff55592a
DanylkoMykola <danylkomykola@gmail.com>
2020-12-14 22:22:38 +0300

    Delete FileResponse

--- src/main/java/com/danylko/yourburger/controller/ProductController.java
+++ src/main/java/com/danylko/yourburger/controller/ProductController.java
@@ -1,24 +1,17 @@
 package com.danylko.yourburger.controller;
 
-import com.danylko.yourburger.entities.FileResponse;
 import com.danylko.yourburger.entities.Product;
 import com.danylko.yourburger.service.ProductService;
 import com.danylko.yourburger.service.StorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.Valid;
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
-import java.util.UUID;
 
 
 @Controller
@@ -52,14 +45,9 @@ public class ProductController {
         product.setName(name);
         product.setDescription(description);
         product.setPrice(Integer.parseInt(price));
-
         logger.info("Start method saveProd");
         logger.info(product.toString());
         String fileName = storageService.store(file);
-        String uri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/download/")
-                .path(name)
-                .toUriString();
         logger.info("File Saved");
         product.setImage(fileName);
         productService.save(product);
